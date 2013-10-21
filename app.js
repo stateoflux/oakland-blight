925f9b1aa1ca49a36e934bae45a4015f82bf4c99
Wayne Montague <wayne.montague@gmail.com>
2013-10-21 09:37:49 -0700

    able to query seeclickfix's api for a list of illegal dumping issues

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
+"use strict";
+
 $(document).ready(function() {
   var masonry;
-  var container = document.querySelector('#container');
+  var container = document.querySelector("#container");
+  var url_comps = [
+    "http://seeclickfix.com/api/issues.json",
+    "?at=Oakland,+CA",
+    "&zoom=10",
+    "&start=24",
+    "&end=0",
+    "&page=1",
+    "&num_results=10",
+    "&search=illegal+dumping",
+    "&callback=?"
+  ];
+
+  $.getJSON(url_comps.join(""), function(data) {
+    console.log(data);
+  });
+
+
+  /* Init Masonry once the images are loaded
+   * ======================================================================= */
 
   imagesLoaded(container, function() {
     masonry = new Masonry(container, {
-      columnWidth: '.grid-sizer',
-      itemSelector: '.item',
+      columnWidth: ".grid-sizer",
+      itemSelector: ".item",
       gutter: 14
     });
   });
