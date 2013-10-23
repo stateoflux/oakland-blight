4d8a81e395dd390200eddc7ca97c47ded058069c
Wayne Montague <wayne.montague@gmail.com>
2013-10-23 10:38:56 -0700

    get issues from seeclickfix endpont and extracts the urls. adds a handlebar template to generate an issue view.

--- app.js
+++ app.js
@@ -3,11 +3,11 @@
 $(document).ready(function() {
   var masonry;
   var container = document.querySelector("#container");
-  var url_comps = [
+  var list_url = [
     "http://seeclickfix.com/api/issues.json",
     "?at=Oakland,+CA",
     "&zoom=10",
-    "&start=24",
+    "&start=48",
     "&end=0",
     "&page=1",
     "&num_results=10",
@@ -15,8 +15,36 @@ $(document).ready(function() {
     "&callback=?"
   ];
 
-  $.getJSON(url_comps.join(""), function(data) {
-    console.log(data);
+  var issue_url = function(id) {
+    var url = "http://seeclickfix.com/api/issues/";
+
+    return url + id + ".json?callback=?";
+  };
+
+  // compile issue template
+  var issueTemplate = Handlebars.compile($('#issue-template').html());
+
+  // var issues = [];
+
+  $.getJSON(list_url.join(""), function(data) {
+    data.forEach(function(issue) {
+      $.getJSON(issue_url(issue.issue_id), function(data) {
+        // issues.push(data);
+        // how should I process each issue object?
+        // for now, all I want is the url to the issue's photo
+        // once i get my urls, how should i proceed?
+        // i suppose I should add list elements programmatically
+        // at each iteration to a document fragment.  at completion
+        // of the loop, I will then insert the fragment into the
+        // main document.  This should kick of the image download
+        // process.  imagesLoaded will then tell masonry to layout.
+        // I should probably use handlerbars for the photo template.
+        console.log(data[0]);
+        $('#container').append(issueTemplate({
+          issue_photo_url: data[0].public_filename
+        }));
+      });
+    });
   });
 
 
